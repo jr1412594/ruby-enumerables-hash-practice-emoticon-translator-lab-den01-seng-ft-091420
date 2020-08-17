e08760f6809f350dbf918de1d1f3f85fe28a8e7c
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-17 00:17:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -42,6 +42,7 @@ def get_japanese_emoticon(file, emoticon)
       final_result = result[emoticon]
       if final_result == nil
         final_result = "Sorry, that emoticon was not found"
+      end
      #binding.pry
     end
     final_result
